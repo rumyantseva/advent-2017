65689d282be7e6a7ec38ec254605811fd3bef784
Elena Grahovac <elena@grahovac.me>
2017-12-10 15:07:43 +0100

    Step 02. Add a loger

--- main.go
+++ main.go
@@ -2,14 +2,19 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
 // How to try it: go run main.go
 func main() {
+	log.Print("Starting the service...")
+
 	http.HandleFunc("/home", func(w http.ResponseWriter, _ *http.Request) {
 		fmt.Fprint(w, "Hello! Your request was processed.")
 	},
 	)
-	http.ListenAndServe(":8000", nil)
+
+	log.Print("The service is ready to listen and serve.")
+	log.Fatal(http.ListenAndServe(":8000", nil))
 }
